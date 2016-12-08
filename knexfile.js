aa340e2e675acf36a88a4eaffd1b27da7d6808ee
Chris Houdlette <choudlette@gmail.com>
2016-12-08 16:28:29 -0700

    fixed env

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: database_URL + "?ssh=true",
+    connection: process.env.DATABASE_URL + "?ssl=true",
     pool: {
       min: 2,
       max: 10
